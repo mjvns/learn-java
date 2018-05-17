464bd88a9fe7612f8ee0b485aa9841adc6b63f84
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2018-05-17 17:29:38 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,9 @@
+class p2 {
+    /*This is a
+    block comment*/
+    public static void main(String args[]) {
+        System.out.println("Hello World One"); //This is a comment
+        //System.out.println("Hello World Two");
+        System.out.println("Hello World Three");
+    }
+}
