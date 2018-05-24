5d757b37183d748bcd2416d23862f2bc55443892
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2018-05-25 02:12:06 +0530

    Add files via upload

---
+++ p6.java
@@ -0,0 +1,10 @@
+class p6 {
+    // this is instance area
+    int a = 5; // instance variables
+    int b = 6;
+    public static void main(String args[]){ // this is static area
+        p6 object = new p6();
+        System.out.println(object.a);
+        System.out.println(object.b);
+    }
+}
---
+++ p7.java
@@ -0,0 +1,12 @@
+class p7 {
+    void m1(){
+        int a = 3; // local variable
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    public static void main(String args[]){
+        p7 obj = new p7();
+        obj.m1();
+    }
+}
